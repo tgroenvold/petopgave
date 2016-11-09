b5d57b61c2add09a5ca3e36163d5e518c7b70758
john doe <john@tekst.com>
2016-11-09 13:05:17 +0100

    pets opgave

---
+++ pets/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Pet
+    {
+        private Specie specie;
+        private string name;
+
+        public Pet(Specie specie, string name )
+
+        {
+            this.name = name;
+            this.specie = specie;
+
+        }
+    }
+
+
+}
